70f53e7a5621b0f710f55d4bf4f65da40ce43ca3
Nur Islam <nurislam03@Nurs-MacBook-Pro.local>
2019-09-25 11:47:04 +0600

    added model for data

--- main.go
+++ main.go
@@ -7,6 +7,23 @@ import (
 	"github.com/go-chi/chi"
 )
 
+// Book struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Init books var as a slice Book struct
+var books []Book
+
 func indexGet(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Getting Index route!"))
 }
@@ -24,9 +41,9 @@ func indexRouter() http.Handler {
 
 func main() {
 
-	// data
-	data.Books = append(data.Books, model.Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &model.Author{Firstname: "John", Lastname: "Doe"}})
-	data.Books = append(data.Books, model.Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &model.Author{Firstname: "Steve", Lastname: "Smith"}})
+	// Hardcoded data - @todo: add database
+	books = append(books, Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
 
 	// initializing router
 	r := chi.NewRouter()
