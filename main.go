1115a45d021c31bc91293340522e578faba64831
Nur Islam <nurislam03@Nurs-MacBook-Pro.local>
2019-09-25 11:43:06 +0600

    data added

--- main.go
+++ main.go
@@ -23,9 +23,16 @@ func indexRouter() http.Handler {
 }
 
 func main() {
+
+	// data
+	data.Books = append(data.Books, model.Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &model.Author{Firstname: "John", Lastname: "Doe"}})
+	data.Books = append(data.Books, model.Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &model.Author{Firstname: "Steve", Lastname: "Smith"}})
+
+	// initializing router
 	r := chi.NewRouter()
 	r.Mount("/index", indexRouter())
 
+	// starting server
 	log.Println("Server Running at port: 8080")
 	http.ListenAndServe(":8080", r)
 }
